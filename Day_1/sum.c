22ec6ae89df85427fd67919c8f9c2d6018a79ad9
Nitin Shukla <nitinshukla122000@gmail.com>
2026-05-28 23:07:47 +0530

    Initial commi

---
+++ Day_1/sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{  int n,i,s=0;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+      s=s+i;
+    {
+        printf("%d",s);
+   
+    }
+return 0;
+}
