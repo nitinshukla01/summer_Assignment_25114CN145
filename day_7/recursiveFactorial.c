e6668d774fadbbe22416be35926d380e0da1fe78
Nitin Shukla <nitinshukla122000@gmail.com>
2026-06-08 19:49:33 +0530

    day 8

---
+++ day_7/recursiveFactorial.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+{
+    if(n<=1)
+    return 1;
+    return n * factorial(n - 1);
+}
+int main()
+{
+    int num;
+    printf("Enter a positive integer: ");
+
+
+    
+}
+
+
+
+    return 0;
+}
