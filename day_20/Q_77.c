ae4df5763db4d78909ba6af8434ffe127c49c640
Nitin Shukla <nitinshukla122000@gmail.com>
2026-06-20 19:55:36 +0530

    day 20 done

---
+++ day_20/Q_77.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    int a[10][10],b[10][10],c[10][10],i,j,r1,r2,c1,c2,k;
+    printf("Enter the first matrix dimensions:");
+    scanf("%d%d",&r1,&c1);
+    
+    printf("Enter the second matrix dimensions:");
+    scanf("%d%d",&r2,&c2);
+
+    printf("Enter the first matrix:");
+    for(i=0;i<r1;i++)
+    {
+        for(j=0;j<c2;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+  printf("Enter the second matrix:");
+  for(i=0;i<r2;i++)
+  {
+    for(j=0;j<c2;j++)
+    {
+        scanf("%d",&b[i][j]);
+    }
+  }  
+    for(i=0;i<r1;i++)
+    {
+       for(j=0;j<c2;j++)
+        {
+            c[i][j]==0;
+          for(k=0;k<c1;k++)
+          {
+            c[i][j]=a[i][k]*b[k][j];
+          }  
+        }
+        
+    }
+   printf("The resultant of matrix:");
+   for(i=0;i<r1;i++)
+   {
+    for(j=0;j<c2;j++)
+    {
+      printf("%d",c[i][j]);
+    }
+     printf("\n");
+   }
+   return 0;
+
+}
