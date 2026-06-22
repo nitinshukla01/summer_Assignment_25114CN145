35e990ce86faa7961a612aaaf18e6ef66c796e22
Nitin Shukla <nitinshukla122000@gmail.com>
2026-06-22 19:20:19 +0530

    day 21 done

---
+++ day_21/Q-81.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  char str[100];
+  int i = 0;
+
+  printf("Enter a string: ");
+  fgets(str,sizeof(str),stdin);
+
+  while(str[i] != '\0')
+    i++;
+  if(str[i-1] == '\n')
+    i--;
+    
+  printf("Length = %d",i);
+
+    return 0;
+}
